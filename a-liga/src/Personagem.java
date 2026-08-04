2c8cf7c84fbc70d9f695393abe4cb28629da31ea
RubensBava <102265511+RubensBava@users.noreply.github.com>
2026-08-04 20:51:53 -0300

    Update Personagem.java

--- a-liga/src/Personagem.java
+++ a-liga/src/Personagem.java
@@ -1,7 +1,23 @@
 public abstract class Personagem {
-    public String nome;
-    public enum sexo { MULHER, HOMEM, OUTRO, PREFIRO_NAO_DIZER };
-    public double altura;
+    private String nome;
+    private enum sexo { MULHER, HOMEM, OUTRO, PREFIRO_NAO_DIZER };
+    private double altura;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
 
     public abstract String apresentar();
 }
