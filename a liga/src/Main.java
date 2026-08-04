aaaaad907acb02069be7a1e6d0ee1fa63a822e4f
RubensBava <102265511+RubensBava@users.noreply.github.com>
2026-08-04 20:09:02 -0300

    boa boa
    muito boa

---
+++ a liga/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        SuperHeroi h = new SuperHeroi();
+        h.nome = "Lisiane Reips";
+        h.sexo = 1;
+        h.altura = 1.75;
+        h.identidadeSecreta = true;
+        h.habilidades = new String[]{"super força", "visão de raio-x", "voar", "super velocidade", "sopro congelante"};
+        h.mostrar();
+
+        // vilao consegue fazer isso pq?
+        h.altura = -300;
+        h.nome = "";
+        h.mostrar();
+    }
+}
